85ce44e22d5bdb5703ab5ddb66724b90eb5347c4
elmergustavo <elmergustavo79@gmail.com>
2021-11-05 20:47:59 -0600

    login implementado

--- JS/controladorEventos/postController.js
+++ JS/controladorEventos/postController.js
@@ -112,16 +112,18 @@ $("#btnMisPost").click(function (e) {
   // crearPost('1', 'Acta de constitución', 'Documento en el que se encuentra de forma resumida los datos y componentes clave de la fase de iniciación del proyecto como lo pueden ser el Alcance, Objetivos o Stakeholders.', null, null)
 });
 
-$("#btnInicioSesion").click(function (e) {
-  e.preventDefault();
-  accesoCorreo("pepito2@gmail.com", "12345678");
-  // registroCorreo(
-  //   "pepito3@gmail.com",
-  //   "12345678",
-  //   "Gustavo",
-  //   "Tavo",
-  //   "https://images.unsplash.com/photo-1633121919063-471d6534a2e1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1974&q=80"
-  // );
+// $("#btnInicioSesion").click(function (e) {
+//   e.preventDefault();
+//   accesoCorreo("pepito2@gmail.com", "12345678");
+//   $('#modalSesion').toggleClass('active');
+//   alert('entro')
+//   registroCorreo(
+//     "pepito3@gmail.com",
+//     "12345678",
+//     "Gustavo",
+//     "Tavo",
+//     "https://images.unsplash.com/photo-1633121919063-471d6534a2e1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1974&q=80"
+//   );
   
-  //  accesFaccebook();
-});
+//    accesFaccebook();
+// });
